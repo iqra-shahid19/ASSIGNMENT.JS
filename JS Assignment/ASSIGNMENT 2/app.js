fff0b47b67bfb28e98d605be7360cda1b4a1d3e0
iqra-shahid19 <128931791+iqra-shahid19@users.noreply.github.com>
2023-05-15 21:05:48 +0500

    Add files via upload

---
+++ JS Assignment/ASSIGNMENT 2/app.js
@@ -0,0 +1,54 @@
+// ------------------QUESTION NO 1----------------
+// Declare a variable called username
+// var userName = "Jhone Doe";
+
+// ------------------QUESTION N0 2----------------
+// Declare a variable called myName & assign to it a string that represents your Full Name
+// var myName = "Iqra Shahid";
+
+// -------------------QUESTION N0 3---------------
+// Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box
+// var message = "Hello World";
+// alert(message);
+
+// --------------QUESTION N0 4---------------------
+// Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// var name = "Jhon e Doe ";
+// var age  = "18 years old  ";
+// var skill = "Certified mobile application development";
+// alert(name);
+// alert(age);
+// alert(skill);
+
+// ---------------------QUESTION N0 5------------------
+// Write a script to display the following alert using one JS variable:
+// var cut = "pizza \n pizz \n piz \n pi \n p"
+// alert(cut);
+
+// -----------------QUESTION NO 6----------------------
+// Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+// var email = "example@example.com";
+// alert("My email address is "+ email);
+
+// ---------------------QUESTION NO 7--------------------
+// Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+// var book = "A Smarter way to learn javasript ";
+// alert(book)
+
+// ----------------------QUESTION NO 8--------------------
+// Write a script to display this in browser through JS
+// var s = "Yah! I can write HTML content through JavaScript"
+// document.write(s);
+
+// ----------------------QUESTION NO 9---------------------
+// Store following string in a variable and show in alert and browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+// var style = " “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” \n\n "
+// alert(style);
+// document.write(style);
+
---
+++ Javascipt assignment/Assignment 1/app.js
@@ -0,0 +1,20 @@
+// -------------------QUESTION NO 1----------------------
+// Write a script to greet your website visitor using JS alert box.
+// alert("welcome to our website!");
+
+// ---------------------QUESTION NO 2----------------------
+// Write a script to display following message on your web page.
+// alert("JavaScript Alert  \n\n Error! please enter a valid password.")
+
+// ----------------------QUESTION NO 3-----------------------
+// Write a script to display following message on your web page (Hint : Use line break)
+// alert("JavaScript Alert \n\n Welcome to JS Land... \n Happy Coding!")
+
+// ------------------------QUESTION N0 4------------------------
+// Write a script to display following messages in sequence
+// alert("JavaScript Alert \n\n Welcome to JS Land...");
+// alert("JavaScript Alert \n\n Happy Coding!");
+
+// --------------------QUESTION NO 5-------------------------
+// Generate the following message through browser’s developer console
+// alert("JavaScript Alert \n\n Hello... I can run JS through my web browser's console");
